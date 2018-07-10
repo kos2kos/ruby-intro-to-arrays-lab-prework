6e89b5b0d8c87976a706d9b7a08a9927ccf642a2
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-10 15:08:09 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,4 +2,21 @@ def instantiate_new_array
   return []
 end
 
-def ar
+def array_with_two_elements
+  return [1,2]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  
